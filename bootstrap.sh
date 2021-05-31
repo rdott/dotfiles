1c4754024a80dcc11238968aaa31f73b0eb8e232
Richard Todd <richard.todd@shopify.com>
2021-05-31 14:20:34 -0400

    Initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+# Only run nested steps in Spin workspaces.
+if [[ "$SPIN" = 1 ]]
+then
+  # Only run nested steps in Spin + shopify/shopify workspaces.
+  if [[ "$SPIN_REPO_SOURCE_PATH" = "/src/github.com/shopify/shopify" ]]
+  then
+    cartridge insert rdott/fp-merchant-metafields
+    . /cartridges/fp-merchant-metafields/setup.sh
+  fi
+fi
