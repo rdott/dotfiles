aba54516aae30b7be1c2a984e97a4d9c9021d741
Richard T <richard.todd@shopify.com>
2022-01-21 11:21:39 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,9 @@
 # Only run nested steps in Spin workspaces.
-if [[ "$SPIN" = 1 ]]
-then
+if [[ "$SPIN" ]]; then
   # Only run nested steps in Spin + shopify/shopify workspaces.
   if [[ "$SPIN_REPO_SOURCE_PATH" = "/src/github.com/shopify/shopify" ]]
   then
-    cartridge insert rdott/inventory-states
+    iso cartridge insert rdott/inventory-states
     . /cartridges/inventory-states/setup.sh
   fi
 fi
