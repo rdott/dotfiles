9a23134ae79dd05eee260082b78fefa024f4e5ba
Richard Todd <richard.todd@shopify.com>
2022-01-21 12:01:10 -0500

    Removed conditional

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,6 @@
 # Only run nested steps in Spin workspaces.
 if [[ "$SPIN" ]]; then
   # Only run nested steps in Spin + shopify/shopify workspaces.
-  if [[ "$SPIN_REPO_SOURCE_PATH" = "/src/github.com/shopify/shopify" ]]
-  then
     iso cartridge insert rdott/inventory-states
     . /cartridges/inventory-states/setup.sh
-  fi
 fi
